04c27edb1dc7385f977859c69c0bad55b9ceb483
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-04-24 13:28:09 +0900

    :snowflake: Add pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -4,22 +4,37 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.git-hooks.flakeModule ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
       flake = {
         homeManagerModules.default = ./nix/hm-module.nix;
         darwinModules.default = ./nix/darwin-module.nix;
       };
-      perSystem = { pkgs, lib, ... }: {
+      perSystem = { config, pkgs, lib, ... }: {
         packages = lib.optionalAttrs pkgs.stdenv.isDarwin {
           default = pkgs.callPackage ./nix/package.nix { };
         };
 
+        pre-commit.settings.hooks = {
+          golangci-lint.enable = true;
+          gofumpt = {
+            enable = true;
+            entry = "${pkgs.gofumpt}/bin/gofumpt -w";
+            types = [ "go" ];
+          };
+        };
+
         devShells.default = pkgs.mkShell {
+          inputsFrom = [ config.pre-commit.devShell ];
           packages = with pkgs; [
             git
             go
